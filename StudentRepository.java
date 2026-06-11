f0e8e6ee28122755411562259dcf0b52c48cc590
Mamatha47329 <jarpulamamatha57@gmail.com>
2026-06-11 12:21:12 +0530

    Create StudentRepository.java

---
+++ StudentRepository.java
@@ -0,0 +1,12 @@
+package com.pms.repository;
+
+import com.pms.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByEmail(String email);
+
+}
